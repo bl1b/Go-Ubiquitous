1c1fa7b0f1f20340f38445ef1b7df8e5ec33c8eb
Jan Grünewald <jan.gruenewald84@googlemail.com>
2016-12-28 17:34:23 +0100

    Receive incoming data-item

--- sunshinewatchface/src/main/java/com/example/android/sunshine/UpdateWeatherDataService.java
+++ sunshinewatchface/src/main/java/com/example/android/sunshine/UpdateWeatherDataService.java
@@ -2,11 +2,16 @@ package com.example.android.sunshine;
 
 import android.util.Log;
 
+import com.example.android.sunshine.common.data.WeatherContract;
 import com.example.android.sunshine.common.utilities.LogHelper;
+import com.google.android.gms.common.data.FreezableUtils;
+import com.google.android.gms.wearable.DataEvent;
 import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.MessageEvent;
-import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
 import com.google.android.gms.wearable.WearableListenerService;
+
+import java.util.List;
 
 /**
  * Created by Jan-2 on 27.12.2016.
@@ -17,33 +22,25 @@ public class UpdateWeatherDataService extends WearableListenerService {
     private final static String TAG = LogHelper.LOG_TAG(UpdateWeatherDataService.class);
 
     @Override
-    public void onCreate() {
-        Log.d(TAG, "Created UpdateWeatherDataService");
-        super.onCreate();
-
-    }
-
-    @Override
     public void onDataChanged(DataEventBuffer dataEventBuffer) {
         Log.d(TAG, "Received changed data.");
-        super.onDataChanged(dataEventBuffer);
-    }
 
-    @Override
-    public void onMessageReceived(MessageEvent messageEvent) {
-        Log.d(TAG, "Message received");
-        super.onMessageReceived(messageEvent);
-    }
+        final List<DataEvent> events = FreezableUtils.freezeIterable(dataEventBuffer);
 
-    @Override
-    public void onPeerConnected(Node node) {
-        Log.d(TAG, "Node connected");
-        super.onPeerConnected(node);
-    }
+        for (DataEvent event : events) {
+            if (event.getDataItem().getUri().getEncodedPath().equals("/weather")) {
+                Log.d(TAG, "Handling event: " + event.getDataItem().getUri());
+                DataMap data = DataMapItem.fromDataItem(event.getDataItem()).getDataMap();
 
-    @Override
-    public void onPeerDisconnected(Node node) {
-        Log.d(TAG, "Node disconnected");
-        super.onPeerDisconnected(node);
+                final long weatherDate = data.getLong(WeatherContract.WeatherEntry.COLUMN_DATE);
+                final float weatherMinTemp = data.getFloat(WeatherContract.WeatherEntry.COLUMN_MIN_TEMP);
+                final float weatherMaxTemp = data.getFloat(WeatherContract.WeatherEntry.COLUMN_MAX_TEMP);
+                final float weatherId = data.getInt(WeatherContract.WeatherEntry.COLUMN_WEATHER_ID);
+
+                continue;
+            }
+
+            Log.w(TAG, "Unhandled event URI: " + event.getDataItem().getUri());
+        }
     }
 }
